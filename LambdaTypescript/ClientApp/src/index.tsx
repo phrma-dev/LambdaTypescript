3774b75a34cce7a2e97f2f863939849436b99464
kdudley <kdudley@phrma.org>
2022-10-13 17:48:03 -0400

    Commit

--- LambdaTypescript/ClientApp/src/index.tsx
+++ LambdaTypescript/ClientApp/src/index.tsx
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +7,7 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl} >
+  <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>,
    document.getElementById("root"));
