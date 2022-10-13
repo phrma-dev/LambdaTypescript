34c96798840bf26f0de08e999895731b9ccf71d2
kdudley <kdudley@phrma.org>
2022-10-13 17:22:34 -0400

    d

---
+++ LambdaTypescript/ClientApp/src/App.tsx
@@ -0,0 +1,31 @@
+﻿import * as React from 'react';
+import Dash from './Pages/Dashboard';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+const App = () => {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/Dash">Dash</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Route path="/Dash" component={Dash} />
+
+      </div>
+    </BrowserRouter>
+
+    );
+  
+}
+
+export default App;
--- LambdaTypescript/ClientApp/src/index.tsx
+++ LambdaTypescript/ClientApp/src/index.tsx
@@ -3,29 +3,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
-import Dash from './Dash';
+import App from './App';
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl} >
-      <div>
-        <ul>
-          <li>
-            <Link to="/Dash">Dash</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <hr />
-        <Route path="/Dash" component={Dash} />
-
-      </div>
+  <App />
   </BrowserRouter>,
    document.getElementById("root"));
 registerServiceWorker();
